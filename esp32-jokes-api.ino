fec6d444ee2463921cee39525d93dabdc2013afb
shivam-dev07 <116815273+shivam-dev07@users.noreply.github.com>
2024-03-04 20:14:24 +0530

    Add files via upload

---
+++ esp32-jokes-api.ino
@@ -0,0 +1,95 @@
+/*
+  ESP32 HTTPClient Jokes API Example
+
+  https://wokwi.com/projects/342032431249883731
+
+  Copyright (C) 2022, Uri Shaked
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+const char* ssid = "Raju";
+const char* password = "thekingsuv1901";
+
+#define BTN_PIN 19
+#define TFT_CS    17
+#define TFT_RST   4
+#define TFT_DC    2
+#define TFT_CLK   18
+#define TFT_MOSI  23
+#define TFT_MISO 
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST);
+
+const String url = "https://v2.jokeapi.dev/joke/Programming";
+
+String getJoke() {
+  HTTPClient http;
+  http.useHTTP10(true);
+  http.begin(url);
+  http.GET();
+  String result = http.getString();
+
+  DynamicJsonDocument doc(2048);
+  DeserializationError error = deserializeJson(doc, result);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return "<error>";
+  }
+
+  String type = doc["type"].as<String>();
+  String joke = doc["joke"].as<String>();
+  String setup = doc["setup"].as<String>();
+  String delivery = doc["delivery"].as<String>();
+  http.end();
+  return type.equals("single") ? joke : setup + "  " + delivery;
+}
+
+void nextJoke() {
+   tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.println("\nLoading joke...");
+
+  String joke = getJoke();
+  tft.setTextColor(ILI9341_GREEN);
+  tft.println(joke);
+}
+
+void setup() {
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
+  WiFi.begin(ssid, password, 6);
+
+  tft.begin();
+  tft.setRotation(1);
+
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    tft.print(".");
+  }
+
+  tft.print("\nOK! IP=");
+  tft.println(WiFi.localIP());
+
+  nextJoke();
+}
+
+void loop() {
+  if (digitalRead(BTN_PIN) == LOW) {
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setCursor(0, 0);
+    nextJoke();
+  }
+
+  delay(100);
+}
